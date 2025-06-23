a0de85e31f3d55b1c8a91923e1ec6e5fe6a5437f
MaromaProgra <programacion@maromapromos.com>
2025-06-23 13:48:56 -0600

    Anexo de logo e icono

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-  // Opcional: Confirmar cuando usuario haga click en botón de descarga
+ // Opcional: Confirmar cuando usuario haga click en botón de descarga
 document.querySelectorAll('.btn-download').forEach(btn => {
 btn.addEventListener('click', e => {
-      // Puedes suprimir alert si no deseas interrupciones
     alert('Gracias por descargar el documento.');
 });
 });
